710c7a7774a0331886757ea237fd91e888903800
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-09-11 21:16:03 -0400

    next-ls 0.10.3 (new formula)
    Closes #1.
    
    Signed-off-by: Mitchell Hanberg <contact@mitchellhanberg.com>

---
+++ next-ls.rb
@@ -0,0 +1,94 @@
+class NextLs < Formula
+  desc "Language server for Elixir that just works"
+  homepage "https://www.elixir-tools.dev/next-ls"
+  url "https://github.com/elixir-tools/next-ls/archive/refs/tags/v0.10.3.tar.gz"
+  sha256 "1cf49f3b6ff50935df942680385fa5a8a2ec80450810279cebf911b9e4237f0f"
+  license "MIT"
+
+  depends_on "elixir" => :build
+  depends_on "xz" => :build
+  depends_on "erlang"
+
+  resource "zig" do
+    on_macos do
+      on_arm do
+        url "https://ziglang.org/download/0.10.0/zig-macos-aarch64-0.10.0.tar.xz"
+        sha256 "02f7a7839b6a1e127eeae22ea72c87603fb7298c58bc35822a951479d53c7557"
+      end
+
+      on_intel do
+        url "https://ziglang.org/download/0.10.0/zig-macos-x86_64-0.10.0.tar.xz"
+        sha256 "3a22cb6c4749884156a94ea9b60f3a28cf4e098a69f08c18fbca81c733ebfeda"
+      end
+    end
+
+    on_linux do
+      url "https://ziglang.org/download/0.10.0/zig-linux-x86_64-0.10.0.tar.xz"
+      sha256 "631ec7bcb649cd6795abe40df044d2473b59b44e10be689c15632a0458ddea55"
+    end
+  end
+
+  def install
+    zig_install_dir = buildpath/"zig"
+    mkdir zig_install_dir
+    resources.each do |r|
+      r.fetch
+
+      system "tar", "xvC", zig_install_dir, "-f", r.cached_download
+      zig_dir =
+        if OS.mac? && Hardware::CPU.arm?
+          zig_install_dir/"zig-macos-aarch64-0.10.0"
+        elsif OS.mac? && Hardware::CPU.intel?
+          zig_install_dir/"zig-macos-x86_64-0.10.0"
+        elsif OS.linux? && Hardware::CPU.intel?
+          zig_install_dir/"zig-linux-x86_64-0.10.0"
+        end
+
+      ENV["PATH"] = "#{zig_dir}:" + ENV["PATH"]
+    end
+
+    system "mix", "local.hex", "--force"
+    system "mix", "local.rebar", "--force"
+
+    target =
+      if OS.mac? && Hardware::CPU.arm?
+        "darwin_arm64"
+      elsif OS.mac? && Hardware::CPU.intel?
+        "darwin_amd64"
+      elsif OS.linux? && Hardware::CPU.intel?
+        "linux_amd64"
+      end
+
+    ENV["BURRITO_TARGET"] = target
+    ENV["MIX_ENV"] = "prod"
+    system "mix", "deps.get"
+    system "mix", "release"
+
+    bin.install "burrito_out/next_ls_#{target}" => "nextls"
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    shell_output("#{Formula["erlang"].prefix}/bin/epmd -daemon")
+
+    Open3.popen3("#{bin}/nextls", "--stdio") do |stdin, stdout, _e, w|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+      Process.kill("KILL", w.pid)
+    end
+  end
+end
