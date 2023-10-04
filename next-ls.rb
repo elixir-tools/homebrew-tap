dcc5e9316bc38ccd45c7bd5350e818ce2de59320
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-10-03 22:11:59 -0400

    next-ls 0.13.2
    Closes #11.
    
    Signed-off-by: Mitchell Hanberg <contact@mitchellhanberg.com>

--- next-ls.rb
+++ next-ls.rb
@@ -1,8 +1,8 @@
 class NextLs < Formula
   desc "Language server for Elixir that just works"
   homepage "https://www.elixir-tools.dev/next-ls"
-  url "https://github.com/elixir-tools/next-ls/archive/refs/tags/v0.13.1.tar.gz"
-  sha256 "57c65ba4ce312db72c8515993473ad5f3bc901eaad73d39b3553ee9a8fae4f8f"
+  url "https://github.com/elixir-tools/next-ls/archive/refs/tags/v0.13.2.tar.gz"
+  sha256 "abdc2490ff6a4350c63fd7a3b5e7cc69bf10ed15b4e7d1fa9b51b35db4b5b563"
   license "MIT"
 
   bottle do
@@ -18,19 +18,19 @@ class NextLs < Formula
   resource "zig" do
     on_macos do
       on_arm do
-        url "https://ziglang.org/download/0.10.0/zig-macos-aarch64-0.10.0.tar.xz"
-        sha256 "02f7a7839b6a1e127eeae22ea72c87603fb7298c58bc35822a951479d53c7557"
+        url "https://ziglang.org/download/0.11.0/zig-macos-aarch64-0.11.0.tar.xz"
+        sha256 "c6ebf927bb13a707d74267474a9f553274e64906fd21bf1c75a20bde8cadf7b2"
       end
 
       on_intel do
-        url "https://ziglang.org/download/0.10.0/zig-macos-x86_64-0.10.0.tar.xz"
-        sha256 "3a22cb6c4749884156a94ea9b60f3a28cf4e098a69f08c18fbca81c733ebfeda"
+        url "https://ziglang.org/download/0.11.0/zig-macos-x86_64-0.11.0.tar.xz"
+        sha256 "1c1c6b9a906b42baae73656e24e108fd8444bb50b6e8fd03e9e7a3f8b5f05686"
       end
     end
 
     on_linux do
-      url "https://ziglang.org/download/0.10.0/zig-linux-x86_64-0.10.0.tar.xz"
-      sha256 "631ec7bcb649cd6795abe40df044d2473b59b44e10be689c15632a0458ddea55"
+      url "https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz"
+      sha256 "2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047"
     end
   end
 
@@ -43,11 +43,11 @@ class NextLs < Formula
       system "tar", "xvC", zig_install_dir, "-f", r.cached_download
       zig_dir =
         if OS.mac? && Hardware::CPU.arm?
-          zig_install_dir/"zig-macos-aarch64-0.10.0"
+          zig_install_dir/"zig-macos-aarch64-0.11.0"
         elsif OS.mac? && Hardware::CPU.intel?
-          zig_install_dir/"zig-macos-x86_64-0.10.0"
+          zig_install_dir/"zig-macos-x86_64-0.11.0"
         elsif OS.linux? && Hardware::CPU.intel?
-          zig_install_dir/"zig-linux-x86_64-0.10.0"
+          zig_install_dir/"zig-linux-x86_64-0.11.0"
         end
 
       ENV["PATH"] = "#{zig_dir}:" + ENV["PATH"]
