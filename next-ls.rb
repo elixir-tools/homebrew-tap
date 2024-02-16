e72b7a7be1189bb8732f4f8aad6e8070b7c4f0f7
Mitchell Hanberg <contact@mitchellhanberg.com>
2024-02-16 17:22:47 +0000

    next-ls: update 0.17.0 bottle.

--- next-ls.rb
+++ next-ls.rb
@@ -6,9 +6,10 @@ class NextLs < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/elixir-tools/homebrew-tap/releases/download/next-ls-0.15.0"
-    sha256 cellar: :any_skip_relocation, ventura:      "c9b48f5659dc62211a0d76990050a77ba87419bc3a3cee8aeac06b26ba6267f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "671d7ad6205852da11d017b224fc2e707d8c66ecd2905edea4fd23880161aac7"
+    root_url "https://github.com/elixir-tools/homebrew-tap/releases/download/next-ls-0.17.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "836c978e4a5059e8531db4606a2a2d9ba2110878d3e2331d4253ff1bf4cbc291"
+    sha256 cellar: :any_skip_relocation, ventura:      "cb77027aaa7b459e1a49dd78a867e74b95aa9135d8a818dcac82d723fc5a91e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5c9e2eee61a1a215679aa96403cb586febf8d77bee274fa8308a2d7f86118974"
   end
 
   depends_on "elixir" => :build
